2c09938e191940ab643e55023b21c964c0a71cee
wrdowney <willd20@vt.edu>
2022-07-12 11:51:40 -0400

    Complete exercise1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,30 @@ const Button = ({onClick, text}) => {
   );
 }
 
+const Statistics = ({good, neutral, bad}) => {
+  if(good === 0 && neutral === 0 && bad === 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>All: {good + neutral + bad}</p>
+        <p>Average: {(good - bad) / (good + neutral + bad)}</p>
+        <p>Positive: {(good) / (good + neutral + bad)}</p>
+      </div>
+    );
+  }
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -18,13 +42,7 @@ const App = () => {
       <Button onClick={() => setGood(good + 1)} text='Good' />
       <Button onClick={() => setNeutral(neutral + 1)} text='Neutral' />
       <Button onClick={() => setBad(bad + 1)} text='Bad' />
-      <h1>Statistics</h1>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
-      <p>All: {good + neutral + bad}</p>
-      <p>Average: {(good - bad) / (good + neutral + bad)}</p>
-      <p>Positive: {(good) / (good + neutral + bad)}</p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
