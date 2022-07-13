0fbe76d9d0360e2329d1a9d73041434c42a02436
Will Downey <willd20@vt.edu>
2022-07-13 19:51:38 -0400

    Complete exercise 2.12

--- part2/data-for-countries/src/App.js
+++ part2/data-for-countries/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'; 
+import axios from 'axios';
+import CountryDisplay from './components/CountryDisplay';
 
 function App() {
+
+  const [searchQuery, setSearchQuery] = useState('botswana');
+  const [searchData, setSearchData] = useState('');
+
+  const updateSearchCountries = (e) => {
+    e.preventDefault();
+    setSearchQuery(e.target.value);
+  }
+
+  // console.log(`https://restcountries.com/v2/name/${searchQuery}`);
+  useEffect(() => {
+    axios
+    .get(`https://restcountries.com/v2/name/${searchQuery}`)
+    .then(response => {
+      setSearchData(response.data);  
+    });
+  }, [searchQuery]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      find countries<input value={searchQuery} onChange={updateSearchCountries} />
+      <CountryDisplay searchData={searchData} />
     </div>
   );
 }
---
+++ part2/data-for-countries/src/components/CountryDisplay.js
@@ -0,0 +1,43 @@
+const CountryDisplay = ({searchData}) => {
+    console.log(searchData);
+    if (searchData.length > 10) {
+        return (
+            <p>Too many matches, specify another filter</p>
+        );
+    }
+    else if (searchData.length > 1) {
+        return (
+            <div>
+                {searchData.map(country => 
+                    <p>{country.name}</p>
+                )}
+            </div>
+            
+        );
+    }
+    else if (searchData.length > 0) {
+        const country = searchData[0];
+        console.log(country.name);
+        return (
+            <div>
+                <h1>{country.name}</h1>
+                <p>capital {country.capital}</p>
+                <p>area {country.area}</p>
+                <h3>languages:</h3>
+                <ul>
+                    {country.languages.map((language, i) => 
+                        <li key={i}>{language.name}</li>
+                    )}
+                </ul>
+                <img src={country.flags.png} alt='Country flag'/>
+            </div>
+        );
+    }
+    else {
+        return (
+            <p>No countries found matching your search</p>
+        );
+    }
+};
+
+export default CountryDisplay;
