a4f4adf549210768b3454dc96fb341d5285d825d
wrdowney <willd20@vt.edu>
2022-07-14 08:54:10 -0400

    Working on exercise 2.14

--- part2/data-for-countries/src/components/CountryInformation.js
+++ part2/data-for-countries/src/components/CountryInformation.js
@@ -1,11 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+
+const api_key = process.env.REACT_APP_API_KEY;
 
 const CountryInformation = ({country}) => {
     const [showInformation, setShowInformation] = useState(false);
+    const [weatherData, setWeatherData] = useState();
 
     const toggleShowInformation = () => {
         setShowInformation(!showInformation);
     }
+
+    console.log(country);
+    console.log(country.latlng[0]);
+
+    useEffect(() => {
+        axios
+        .get(`https://api.openweathermap.org/data/3.0/onecall?lat=${country.latlng[0]}&lon=${country.latlng[1]}&exclude={part}&appid=${api_key}`)
+        .then(response => {
+          console.log(response);  
+        });
+      }, [country.latlng]);
 
     if (!showInformation) {
         return(
@@ -16,21 +31,24 @@ const CountryInformation = ({country}) => {
             
         );
     }
-    return (
-        <div>
-            <h1>{country.name}</h1>
-            <button onClick={toggleShowInformation}>hide</button>
-            <p>capital {country.capital}</p>
-            <p>area {country.area}</p>
-            <h3>languages:</h3>
-            <ul>
-                {country.languages.map((language, i) => 
-                    <li key={i}>{language.name}</li>
-                )}
-            </ul>
-            <img src={country.flags.png} alt='Country flag'/>
-        </div>
-    );
+    else {
+        return (
+            <div>
+                <h1>{country.name}</h1>
+                <button onClick={toggleShowInformation}>hide</button>
+                <p>capital {country.capital}</p>
+                <p>area {country.area}</p>
+                <h3>languages:</h3>
+                <ul>
+                    {country.languages.map((language, i) => 
+                        <li key={i}>{language.name}</li>
+                    )}
+                </ul>
+                <img src={country.flags.png} alt='Country flag'/>
+            </div>
+        );
+    }
+
 }
 
 export default CountryInformation;
