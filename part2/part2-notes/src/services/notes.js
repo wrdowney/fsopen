50ffdcc5ad6f99fde7efa6447d8fb22ff99deeee
Will Downey <willd20@vt.edu>
2022-07-14 20:35:31 -0400

    refactor(services): Clean exports

--- part2/part2-notes/src/services/notes.js
+++ part2/part2-notes/src/services/notes.js
@@ -6,7 +6,7 @@ const getAll = () => {
   return request.then(response => response.data)
 }
 
-const create = newObject => {
+const create = (newObject) => {
   const request = axios.post(baseUrl, newObject)
   return request.then(response => response.data)
 }
@@ -16,8 +16,4 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-export default { 
-  getAll: getAll, 
-  create: create, 
-  update: update 
-}
+export default { getAll, create, update }
