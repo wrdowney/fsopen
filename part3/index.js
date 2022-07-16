a1b7cec3fa8d69a387195ac95010382b51f6610a
Will Downey <willd20@vt.edu>
2022-07-16 15:02:38 -0400

    feat: use express for routes

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,5 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
 let notes = [
     {
@@ -21,12 +22,16 @@ let notes = [
     }
   ]
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(notes));
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>');
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+const PORT = 3002
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
