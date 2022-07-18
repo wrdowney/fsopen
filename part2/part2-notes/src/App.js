163a0158c043058c98961efe4b23ffbe3f078abf
Will Downey <willd20@vt.edu>
2022-07-18 17:00:41 -0400

    Changes to part3-backend submodule

--- part2/part2-notes/src/App.js
+++ part2/part2-notes/src/App.js
@@ -43,15 +43,17 @@ const App = () => {
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
     const changedNote = { ...note, important: !note.important }
+    console.log(changedNote);
   
     noteService
       .update(id, changedNote)
       .then(returnedNote => {
+        console.log(returnedNote);
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
         setErrorMessage(
-          `Note '${note.content}' was already removed from server`
+          error
         )
         setTimeout(() => {
           setErrorMessage(null)
