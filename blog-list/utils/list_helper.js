3860bdb4afc37110bd50a1da81b8319549573809
Will Downey <willd20@vt.edu>
2022-07-23 19:52:33 -0400

    Complete exercise 4.5

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -2,13 +2,26 @@ const dummy = (blogs) => {
   return 1;
 }
 
-const totalLikes = (posts) => {
-  return posts.reduce((sum, post) => {
-    return sum + post.likes;
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes;
   }, 0);
+}
+
+const favoriteBlog = (blogs) => {
+  mostLikes = 0
+  mostLikedBlog = null;
+  blogs.forEach(blog => {
+    if(blog.likes > mostLikes) {
+      mostLikedBlog = blog;
+      mostLikes = blog.likes;
+    }
+  })
+  return mostLikedBlog
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
